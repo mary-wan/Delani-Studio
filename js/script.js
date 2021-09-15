fee8dcc976be80eff6d50019546eae36cb6cd25b
mary-wan <mary.njenga@student.moringaschool.com>
2021-09-15 15:16:04 +0300

    add reset function

--- js/script.js
+++ js/script.js
@@ -1,70 +1,78 @@
-$(document).ready(function(){
-    $(".design").click(function(){
+$(document).ready(function () {
+    $(".design").click(function () {
         $(".description1").slideToggle();
         $("#desgn").slideToggle();
     });
-    $(".development").click(function(){
+    $(".development").click(function () {
         $(".description2").slideToggle();
         $("#develop").slideToggle();
     });
-    $(".prdManagement").click(function(){
+    $(".prdManagement").click(function () {
         $(".description3").slideToggle();
         $("#productMngt").slideToggle();
     });
-    $(".work1").mouseover(function(){
+    $(".work1").mouseover(function () {
         $("#work1Desc").show();
     });
-    $(".work1").mouseout(function(){
+    $(".work1").mouseout(function () {
         $("#work1Desc").hide();
     });
-    $(".work2").mouseover(function(){
+    $(".work2").mouseover(function () {
         $("#work2Desc").show();
     });
-    $(".work2").mouseout(function(){
+    $(".work2").mouseout(function () {
         $("#work2Desc").hide();
     });
-    $(".work3").mouseover(function(){
+    $(".work3").mouseover(function () {
         $("#work3Desc").show();
     });
-    $(".work3").mouseout(function(){
+    $(".work3").mouseout(function () {
         $("#work3Desc").hide();
     });
-    $(".work4").mouseover(function(){
+    $(".work4").mouseover(function () {
         $("#work4Desc").show();
     });
-    $(".work4").mouseout(function(){
+    $(".work4").mouseout(function () {
         $("#work4Desc").hide();
     });
-    $(".work5").mouseover(function(){
+    $(".work5").mouseover(function () {
         $("#work5Desc").show();
     });
-    $(".work5").mouseout(function(){
+    $(".work5").mouseout(function () {
         $("#work5Desc").hide();
     });
-    $(".work6").mouseover(function(){
+    $(".work6").mouseover(function () {
         $("#work6Desc").show();
     });
-    $(".work6").mouseout(function(){
+    $(".work6").mouseout(function () {
         $("#work6Desc").hide();
     });
-    $(".work7").mouseover(function(){
+    $(".work7").mouseover(function () {
         $("#work7Desc").show();
     });
-    $(".work7").mouseout(function(){
+    $(".work7").mouseout(function () {
         $("#work7Desc").hide();
     });
-    $(".work8").mouseover(function(){
+    $(".work8").mouseover(function () {
         $("#work8Desc").show();
     });
-    $(".work8").mouseout(function(){
+    $(".work8").mouseout(function () {
         $("#work8Desc").hide();
     });
-    
-    $("#formFeedback").submit(function() {
-        var name = $("#name").val(); 
-        var email = $("#email").val(); 
-        var message = $("#feedback").val(); 
+
+    $("#formFeedback").submit(function () {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var message = $("#feedback").val();
 
         alert(name + " we have received your message. Thank you for reaching out to us. ");
-         });
+        resetFields()
+    });
+
+    function resetFields() {
+        $("input#name").val("");
+        $("input#email").val("");
+        
+    }
+
 });
